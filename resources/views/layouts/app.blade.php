e5439453d17ab05c4c902f52fccaa9cb2fdc4277
TIM-JUS <1440406@senati.pe>
2023-11-03 10:52:46 -0500

    Creacion del login

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> User perfil</title>
+ 
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../assets/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../assets/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="{{ route('logout') }}" class="nav-link">Salir</a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+ 
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+      <img src="../assets/img/LOGO.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light"><br> ACCESORIOS <br> PARA MASCOTAS🐶</span>
+    </a>
+ 
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../assets/img/kenyi.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+      </div>
+ 
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="buscar" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+ 
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-book"></i>
+              <p>
+                Pages
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('product.index') }}" class="nav-link"> 
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>PRODUCTOS</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('product.create') }}" class="nav-link"> 
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>AGREGAR PRODUCTO</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('product.my') }}" class="nav-link"> 
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>MIS PRODUCTOS</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('profile.purchase') }}" class="nav-link"> 
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>COMPRAR</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('profile.index') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>PERFIL</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ route('logout') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>LOGOUT</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    @if(session('success'))
+      <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+      </div>
+    @endif
+    @if(session('error'))
+      <div class="alert alert-danger" role="alert">
+        {{ session('error') }}
+      </div>
+    @endif
+ 
+    @yield('content')
+  </div>
+  
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+</div>
+<!-- ./wrapper -->
+ 
+<!-- jQuery -->
+<script src="../assets/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../assets/js/demo.js"></script>
+</body>
+</html>
